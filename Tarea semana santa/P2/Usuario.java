302393b5aae607f38e0037c1bda9557b95f884fe
luisUcP <161058985+luisUcP@users.noreply.github.com>
2024-04-02 21:43:38 -0500

    Create Usuario.java

---
+++ Tarea semana santa/P2/Usuario.java
@@ -0,0 +1,28 @@
+package Punto2;
+
+public class Usuario {
+    private String nombre;
+    private String cedula;
+
+    public Usuario(String nombre, String cedula) {
+        this.nombre = nombre;
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "nombre='" + nombre + '\'' +
+                ", cedula='" + cedula + '\'' +
+                '}';
+    }
+    
+}
