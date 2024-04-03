a81c725992853833a0108ccde36984de562fc3b0
luisUcP <161058985+luisUcP@users.noreply.github.com>
2024-04-02 21:45:48 -0500

    Create Prestamo.java

---
+++ Tarea semana santa/P2/Prestamo.java
@@ -0,0 +1,45 @@
+package Punto2;
+
+import java.util.Date;
+
+
+public class Prestamo {
+    private Libro libro;
+    private Usuario usuario;
+    private Date fechaPrestamo;
+    private Date fechaDevolucion;
+
+    public Prestamo(Libro libro, Usuario usuario, Date fechaPrestamo, Date fechaDevolucion) {
+        this.libro = libro;
+        this.usuario = usuario;
+        this.fechaPrestamo = fechaPrestamo;
+        this.fechaDevolucion = fechaDevolucion;
+    }
+
+    public Libro getLibro() {
+        return libro;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public Date getFechaPrestamo() {
+        return fechaPrestamo;
+    }
+
+    public Date getFechaDevolucion() {
+        return fechaDevolucion;
+    }
+
+    @Override
+    public String toString() {
+        return "Prestamo{" +
+                "libro=" + libro +
+                ", usuario=" + usuario +
+                ", fechaPrestamo=" + fechaPrestamo +
+                ", fechaDevolucion=" + fechaDevolucion +
+                '}';
+    }
+    
+}
