31508d7836476ae5d2d645442c09e23cb8979c11
luisUcP <161058985+luisUcP@users.noreply.github.com>
2024-04-02 22:31:56 -0500

    Create Proveedor.java

---
+++ Tarea semana santa/P3/Proveedor.java
@@ -0,0 +1,28 @@
+class Proveedor {
+    private String nombre;
+    private String telefono;
+    private String direccion;
+
+    public Proveedor(String nombre, String telefono, String direccion) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.direccion = direccion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+
+    public String toString() {
+        return "Proveedor: " + nombre + ", Teléfono: " + telefono + ", Dirección: " + direccion;
+    }
+}
