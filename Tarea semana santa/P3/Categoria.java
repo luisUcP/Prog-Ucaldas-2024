7508a7d0d15badc3830d1294787d469fd290cbdf
luisUcP <161058985+luisUcP@users.noreply.github.com>
2024-04-02 22:33:08 -0500

    Create Categoria.java

---
+++ Tarea semana santa/P3/Categoria.java
@@ -0,0 +1,21 @@
+class Categoria {
+    private String nombre;
+    private String descripcion;
+
+    public Categoria(String nombre, String descripcion) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public String toString() {
+        return "Categoría: " + nombre + ", Descripción: " + descripcion;
+    }
+}
